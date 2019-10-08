e45f9f466437f53e6afe76f8c29014ce1df059f3
Paul <paulbloomfield.uk@gmail.com>
2019-10-08 23:26:28 +0100

    Import basically working v.0.6

---
+++ html/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// If you cannot access $_ENV vars uncomment the next line to force production mode.
+// $_ENV['PHP_ENV'] = 'production';
+
+$appPath = '../src/bootstrap.php';
+
+ini_set('display_errors', 0);
+
+$fullPath = __DIR__.DIRECTORY_SEPARATOR.$appPath;
+if (!include($fullPath)) {
+    if (isset($_ENV['PHP_ENV']) && $_ENV['PHP_ENV'] === 'production') {
+        echo('Could not load bootstrap.');
+    } else {
+        $realPath = realpath(dirname($fullPath));
+        if ($realpath) {
+            echo(htmlspecialchars('Could not load bootstrap from '.$realPath.'.'));
+        } else {
+            echo(htmlspecialchars('Could not load bootstrap from '.$appPath
+                .' (could not resolve path '.$fullPath.').'));
+        }
+    }
+}
--- src/Scherzo/HttpException.php
+++ src/Scherzo/HttpException.php
@@ -1,26 +1,48 @@
 <?php
-/**
- * This file is part of the Scherzo application framework.
- *
- * @link      https://github.com/paulbloomfield-uk/scherzo
- * @license   [MIT](https://github.com/paulbloomfield-uk/scherzo/blob/master/LICENSE).
- * @copyright Copyright © 2017 [Paul Bloomfield](https://github.com/paulbloomfield-uk).
-**/
+
+declare(strict_types=1);
 
 namespace Scherzo;
 
-/**
- *
-**/
-class HttpException extends Exception {
+use Scherzo\Response;
 
-    protected $status = 500;
+class HttpException extends \RuntimeException {
+    protected $statusCode;
+    protected $info;
 
-    public function getStatus() {
-        return $this->status;
+    public function __construct(int $statusCode = 500, string $message = null) {
+        if ($statusCode >= 400 && isset(Response::$statusTexts[$statusCode])) {
+            $this->statusCode = $statusCode;
+        } else {
+            $this->statusCode = 500;
+        }
+        $code = Response::$statusTexts[$this->statusCode];
+        $message = $message === null ? $code : $message;
+        parent::__construct($message, 0);
+        $this->$code = $code;
     }
 
-    public function setStatus($status) {
-        $this->status = $status;
+    public function setCode(string $code) {
+        $this->code = $code;
+        return $this;
+    }
+
+    public function setInfo($info, $value = null) {
+        if (is_array($info)) {
+            $this->info = $info;
+        } elseif (isset($this->info)) {
+            $this->info[$info] = $value;
+        } else {
+            $this->info = [$info => $value];
+        }
+        return $this;
+    }
+
+    public function getInfo() {
+        return $this->info;
+    }
+
+    public function getStatusCode() {
+        return $this->statusCode;
     }
 }
