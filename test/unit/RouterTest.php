76260af7ef76808b9dd5ce25bd0a1562a73777ef
pb-uk <23248021+pb-uk@users.noreply.github.com>
2023-02-05 10:19:07 +0000

    Tidy autoloading

--- test/unit/RouterTest.php
+++ test/unit/RouterTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace UnitTest;
 
 use PHPUnit\Framework\TestCase;
-use TestFixtures\MockController;
+use Scherzo\Test\TestController;
 use Scherzo\Router;
 use Scherzo\HttpException;
 
@@ -14,16 +14,16 @@ final class RouterTest extends TestCase
     public function testShouldRouteAGetRequest(): void
     {
         $router = new Router([
-            ['GET', '/{id:.+}', [MockController::class, 'getIndex']],
+            ['GET', '/{id:.+}', [TestController::class, 'getId']],
         ]);
 
         [$route, $params] = $router->match('GET', '/123');
 
-        $this->assertEquals(
-            [MockController::class, 'getIndex'],
+        $this->assertSame(
+            [TestController::class, 'getId'],
             $route
         );
-        $this->assertEquals(
+        $this->assertSame(
             ['id' => '123'],
             $params
         );
@@ -32,45 +32,45 @@ final class RouterTest extends TestCase
     public function testShouldThrowAnHttpExceptionWhenNotFound(): void
     {
         $router = new Router([
-            ['GET', '/', [MockController::class, 'getIndex']],
+            ['GET', '/', [TestController::class, 'getIndex']],
         ]);
 
         try {
             $router->match('GET', '/will-not-work');
         } catch (HttpException $e) {
             // All JSON errors should have a title.
-            $this->assertEquals('Not Found', $e->getTitle());
+            $this->assertSame('Not Found', $e->getTitle());
             // All JSON errors should have a descriptive message.
-            $this->assertEquals(
+            $this->assertSame(
                 'Could not find /will-not-work',
                 $e->getMessage()
             );
             // All JSON errors should have a status code.
-            $this->assertEquals(404, $e->getStatusCode());
+            $this->assertSame(404, $e->getStatusCode());
         }
     }
 
     public function testShouldThrowAnHttpExceptionWhenMethodNotAllowed(): void
     {
         $router = new Router([
-            [['POST', 'PUT'], '/', [MockController::class, 'getIndex']],
+            [['POST', 'PUT'], '/', [TestController::class, 'getIndex']],
         ]);
 
         try {
             $router->match('GET', '/');
         } catch (HttpException $e) {
             // All JSON errors should have a title.
-            $this->assertEquals('Method Not Allowed', $e->getTitle());
+            $this->assertSame('Method Not Allowed', $e->getTitle());
             // All JSON errors should have a descriptive message.
-            $this->assertEquals(
+            $this->assertSame(
                 'Method GET not allowed for path /',
                 $e->getMessage()
             );
             // All JSON errors should have a status code.
-            $this->assertEquals(405, $e->getStatusCode());
+            $this->assertSame(405, $e->getStatusCode());
             // Method Not Allowed JSON errors should have extra info.
-            $this->assertEquals(['POST', 'PUT'], $e->getAllowedMethods());
-            $this->assertEquals(
+            $this->assertSame(['POST', 'PUT'], $e->getAllowedMethods());
+            $this->assertSame(
                 ['method' => 'GET', 'path' => '/', 'allowed' => ['POST', 'PUT']],
                 $e->getInfo()
             );
